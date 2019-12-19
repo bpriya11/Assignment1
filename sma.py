2b3b2d5276aab38fda38e36763a69c43cad2eb11
bpriya11 <44028058+bpriya11@users.noreply.github.com>
2019-12-19 11:35:37 +0530

    sma file

---
+++ sma.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 15 17:29:42 2019
+
+@author: DELL
+"""
+
+import pandas as pd
+import numpy as np
+import datetime
+import matplotlib.pyplot as plt
+import pandas_datareader as pdr
+#importing data 
+df = pdr.get_data_yahoo('AAPL', 
+                          start=datetime.datetime(2006, 1, 1), 
+                          end=datetime.datetime(2019, 10, 1))
+df = df['Close']
+#Finding the mean
+sma_mean = df.rolling(window=20).mean()
+#Plotting the data
+plt.figure(figsize=(12,6))
+plt.title('   AAPL Plot      ')
+plt.plot(df, label='AAPL', color='orange')
+plt.show()
+#Plotting the mean
+plt.figure(figsize=(12,6))
+plt.title('   SMA Plot      ')
+plt.plot(sma_mean, label='AAPL 20 Day SMA', color='black')
+plt.show()
+#Plotting the data and mean together
+plt.figure(figsize=(12,6))
+plt.title('AAPL and SMA Plot')
+plt.plot(df, label='AAPL')
+plt.plot(sma_mean, label='AAPL 20 Day SMA', color='magenta')
+plt.legend(loc='upper left')
+plt.show()
